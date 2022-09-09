4b7e6be8cf8a1ae980c8b50c2e9dd782b06ba79e
Caio Felipe <caiofeliperro@gmail.com>
2022-09-09 10:28:19 -0300

    Adicionar um arquivo "principal" com as informações necessárias para rodar todo o código

---
+++ main.py
@@ -0,0 +1,9 @@
+# Aqui atualizamos a database
+import data_migrator
+
+# Aqui importamos os gráficos
+import graphs.average
+import graphs.fertility
+import graphs.migration
+import graphs.population
+import graphs.urban_population
