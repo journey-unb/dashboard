ee90dc04d5071d733978e808e8a0099b0a3ff6a5
kyomi <personal@kyomi.codes>
2022-09-16 11:57:49 -0700

    Consertar indentação do arquivo `graphs/population.py`

--- graphs/population.py
+++ graphs/population.py
@@ -38,23 +38,23 @@ valid_years: list[str | int] = []
 # dos anos a ser mostrado. Na próxima etapa do trabalho, utilizaremos
 # a função "Range Slider" do Dash.
 for year in input("Insira o ano desejado:").split():
-  if year.isnumeric():
-    valid_years.append(int(year))
+    if year.isnumeric():
+        valid_years.append(int(year))
 
 # Caso não insira um ano válido, será mostrado a população de todos os anos.
 # Caso contrário, utilizaremos o filtro de valores para mostrar os dados
 # do ano específico.
 if not valid_years:
-  filtered_year = filtered_df
-  valid_years.append("todos os anos")
+    filtered_year = filtered_df
+    valid_years.append("todos os anos")
 else:
-  filtered_year = filter_values(filtered_df, "year", *valid_years)
+    filtered_year = filter_values(filtered_df, "year", *valid_years)
 
 chart = px.pie( # type: ignore
-  filtered_year,
-  values="population",
-  names="region",
-  labels=labels,
+    filtered_year,
+    values="population",
+    names="region",
+    labels=labels,
 )
 
 # TODO: Utilizar títulos dinâmicos e seguir a PEP-8.
